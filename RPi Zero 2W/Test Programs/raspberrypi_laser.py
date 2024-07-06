a7337b2305c892a7df5ffe545e626b2bd64ac672
Duality <fredericksun12@gmail.com>
2024-07-06 14:06:49 +1200

    added lasers to Rpi

---
+++ RPi Zero 2W/Test Programs/raspberrypi_laser.py
@@ -0,0 +1,23 @@
+import time
+import board
+import busio
+import adafruit_vl53l0x
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor_addresses = [0x08, 0x09, 0x0a]
+
+sensors = []
+for address in sensor_addresses:
+  sensor = adafruit_vl53l0x.VL53L0X(i2c, address=address)
+  sensors.append(sensor)
+
+while True:
+  for i, sensor in enumerate(sensors):
+    distance = sensor.range
+    print(f"Sensor {i+1} (Address: {sensor_addresses[i]}) distance: {distance} mm")
+  time.sleep(1)
+
+
+'''
+  Hence, the need to create a distance vs radius cutout, graphing using desmos
+'''
