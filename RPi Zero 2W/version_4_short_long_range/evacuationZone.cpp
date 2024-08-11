19552ff0e1e5db0c1cdb7c262ddc41904fdecebf
Duality <fredericksun12@gmail.com>
2024-08-11 20:10:51 +1200

    Changed triangle approach method

--- RPi Zero 2W/version_4_short_long_range/evacuationZone.cpp
+++ RPi Zero 2W/version_4_short_long_range/evacuationZone.cpp
@@ -66,20 +66,21 @@ void evacuationZone() {
 
 void searchAndApproach(float kP) {
   unsigned long start = millis();
-  unsigned long waitTime = 1000;
+  unsigned long waitTime = 500;
   unsigned long forwardTime = waitTime + 500;
 
   do {
     Serial.print("    Searching - LONG      ");
     comUpdate();
 
-    run(120, -120);
+    run(130, -130);
 
     Serial.println();
 
   } while(y == -1);
 
   run(0, 0, 500);
+  Serial.println();
 
   do {
     Serial.print("    Searching - LONG+SHORT");
@@ -90,7 +91,7 @@ void searchAndApproach(float kP) {
     turn = constrain(turn, -50, 50);
 
     if(      millis() - start < waitTime    ) run(0, 0);
-    else if( millis() - start < forwardTime ) run(120 + turn, 120 - turn);
+    else if( millis() - start < forwardTime ) run(130 + turn, 130 - turn);
     else                                      start = millis();
 
     Serial.println();
@@ -98,6 +99,7 @@ void searchAndApproach(float kP) {
   } while(r == -1 && y != -1);
 
   run(0, 0, 500);
+  Serial.println();
 
   do {
     Serial.print("    Searching - SHORT     ");
@@ -107,7 +109,7 @@ void searchAndApproach(float kP) {
     int turn = error * kP;
     turn = constrain(turn, -50, 50);
 
-    run(120 + turn, 120 - turn);
+    run(130 + turn, 130 - turn);
 
     Serial.println();
 
@@ -116,19 +118,27 @@ void searchAndApproach(float kP) {
   triangleType = victimType;
 
   run(0, 0, 500);
+  Serial.println();
 }
 
 void grabSequence() {
   run(0, 0, 500);
   clawIncrement(500, 1);
 
-  run(150, 150, 3800);
+  run(170, 170, 3800);
   clawIncrement(1200, 2);
+
 
   run(-150, -150, 3000);
   run(0, 0);
-
   clawIncrement(1800, 2);
+
+  do {
+    run(-150, -150);
+    readToF(0);
+  } while(distancesToF[0] < 300);
+
+  run(0, 0);
 
   grabbed = true;
 }
@@ -147,12 +157,18 @@ void findTriangle() {
     triangleType == 0 ? Serial.print("    GREEN") : Serial.print("      RED");
 
 
-    run(125, -125);
+    run(130, -130);
 
     Serial.println();
   } while(triangleX == 9499);
 
   run(0, 0, 500);
+  Serial.println();
+
+  do {
+    readToF(0);
+    run(150, 150);
+  } while(distancesToF[0] > 400);
 
   do {
     Serial.print("    Aligning(R)");
@@ -161,12 +177,25 @@ void findTriangle() {
     triangleType == 0 ? Serial.print("    GREEN") : Serial.print("      RED");
 
 
-    run(120, -120);
+    run(125, -125);
     
     Serial.println();
-  } while(triangleX > 15 && triangleX != 9499);
+  } while(triangleX != 9499);
   
   run(0, 0, 500);
+  Serial.println();
+  unsigned long start = millis();
+
+  do {
+    Serial.print("    Aligning(L)");
+    comUpdate();
+    triangleX = triangleType == 0 ? greenX : redX;
+    triangleType == 0 ? Serial.print("    GREEN") : Serial.print("      RED");
+
+    run(-125, 125);
+
+    Serial.println();
+  } while(triangleX == 9499);
   
   do {
     Serial.print("    Aligning(L)");
@@ -174,21 +203,22 @@ void findTriangle() {
     triangleX = triangleType == 0 ? greenX : redX;
     triangleType == 0 ? Serial.print("    GREEN") : Serial.print("      RED");
 
-    run(-110, 110);
+    run(-125, 125);
 
     Serial.println();
-  } while(triangleX < 5 && triangleX != 9499);
+  } while(triangleX !=9499);
 
-  readToF(0);
+  run(0, 0, 500);
+  Serial.println();
+  unsigned long end = millis();
 
-  if(distancesToF[0] < 400) {
-    run(150, -150, 400);
-  }
+  run(125, -125, (end- start) / 2);
+  run(0, 0);
 }
 
 void dropSequence() {
   while(digitalRead(touchPins[0]) == 1 && digitalRead(touchPins[1]) == 1) {
-    run(200, 200);
+    run(250, 250);
     Serial.println();
   }
 
