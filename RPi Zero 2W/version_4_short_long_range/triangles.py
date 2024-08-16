dd0d4e7f97d648b3c3b561bcc0234cfa456286b1
Duality <fredericksun12@gmail.com>
2024-08-16 18:31:29 +1200

    solved green triangle issues - genearting points in a circle that is centred with the countor, and then identify black elements

--- RPi Zero 2W/version_4_short_long_range/triangles.py
+++ RPi Zero 2W/version_4_short_long_range/triangles.py
@@ -1,22 +1,53 @@
 from config import *
 import cv2
 import numpy as np
+import random
+import math
 
 def findTriangles(image, green, red):
   greenContours, _ = cv2.findContours(green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   redContours, _    = cv2.findContours(red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  image, greenX = positionContour(image, greenContours)
-  image, redX   = positionContour(image, redContours  )
-
-  if redX != 9999: greenX = 9999
+  image, greenX, greenY = positionContour(image, greenContours)
+  image, redX, _        = positionContour(image, redContours  )
+  greenX = verifyGreen(greenX, greenY, green)
   
   return image, greenX, redX
+
+def generateRandomPoints(xCenter, yCenter, radius, numberOfPoints):
+  points = []
+  cycles = 0
+
+  while( len(points) < numberOfPoints and cycles < 15):
+    angle = random.uniform(0, 2 * math.pi)
+
+    r = math.sqrt(random.uniform(0, 1)) * radius
+    x = int(xCenter + r * math.cos(angle))
+    y = int(yCenter + r * math.sin(angle))
+
+    if x < 0 or y < 0 or x > WIDTH - 1 or y > HEIGHT - 1: pass
+    else:                                                 points.append((x, y))
+
+    cycles += 1
+
+  return points
+
+def verifyGreen(greenX, greenY, green):
+  points = generateRandomPoints(greenX, greenY, 7, 10)
+
+  blackCount = 0
+  for point in points:
+    pixel = green[point[1]][point[0]]
+    if pixel == 0: blackCount += 1
+
+  if blackCount < 2: return greenX
+  else:              return 9999
 
 def positionContour(image, contours):
   maxArea = 0
   maxContour = None
-  minX, maxX, averageX = WIDTH, 0, -1
+  minX, maxX, averageX = WIDTH , 0, -1
+  minY, maxY, averageY = HEIGHT, 0, -1
 
   for contour in contours:
     area = cv2.contourArea(contour)
@@ -31,10 +62,14 @@ def positionContour(image, contours):
     for point in maxContour:
       minX = min(minX, point[0][0])
       maxX = max(maxX, point[0][0])
+      minY = min(minY, point[0][1])
+      maxY = max(maxY, point[0][1])
 
     averageX = int((minX + maxX) / 2)
+    averageY = int((minY + maxY) / 2)
 
   else:
     averageX = 9999
+    averageY = 9999
 
-  return image, averageX
+  return image, averageX, averageY
